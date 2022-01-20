fd4482b9dac1a28e178ca53fd54ef1b2c56e20cf
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-01-21 03:16:48 +0600

    File Update
    Main Files Update

---
+++ assets/js/common.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots: false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+});
