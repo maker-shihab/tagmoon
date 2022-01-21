30893e5c8f952ce07f7ad013f7952c1ff239d36e
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-01-21 21:47:28 +0600

    Desktop

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,21 +1,72 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    dots: false,
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:4
-        },
-        1000:{
-            items:6
+    // mobile menu 
+    $('.all-p-humber').click(function(){
+        $(this).toggleClass('open');
+    });	
+
+    // Start Humber Main Menu Icon Js
+    $('.mobile-menu-icon').click(function(){
+        $('nav.main_menu').slideToggle(300);
+        $('nav.main_menu').addClass('mb_dropdown');
+    });
+
+  $('.supply_body').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: false,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1100:{
+                items:4
+            },
+            1200:{
+                items:5
+            }
         }
-    }
-})
+    }) ;
+    $('.brand_area').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: false,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1100:{
+                items:4
+            },
+            1200:{
+                items:6
+            }
+        }
+    });  
+    $('.token_supply').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: false,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1100:{
+                items:4
+            }
+        }
+    });
     $('.s_a_t_1').click(function(){
         $(this).addClass('active');
         $('.s_a_t_2').removeClass('active');
